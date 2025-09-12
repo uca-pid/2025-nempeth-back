6ed15bdebba18db09745d9fdbd50dc78b7bbaf70
Lucas Heredia <lherediamartinelli@gmail.com>
2025-09-12 20:11:06 -0300

    removed smalltalk things and added springboot project

---
+++ src/test/java/com/nempeth/korven/KorvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nempeth.korven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
