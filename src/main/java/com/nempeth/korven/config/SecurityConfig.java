f2a2a13275b212e7f991fcca0702b4828c82841b
Lucas Heredia <lherediamartinelli@gmail.com>
2025-09-26 21:32:29 -0300

    added korven web url

--- src/main/java/com/nempeth/korven/config/SecurityConfig.java
+++ src/main/java/com/nempeth/korven/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(List.of("http://localhost:5173"));
+        cors.setAllowedOrigins(List.of("https://korven-web-c21b9b7375ff.herokuapp.com", "http://localhost:5173"));
         cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         cors.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         cors.setExposedHeaders(List.of("Authorization"));
